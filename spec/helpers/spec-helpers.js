16fb751f55d6b7438121d77d8592a4859ae3ed02
ikngtty <ikngtty@gmail.com>
2018-03-12 00:10:14 +0900

    Improve comments.

--- spec/helpers/spec-helpers.js
+++ spec/helpers/spec-helpers.js
@@ -8,11 +8,10 @@ export default class {
   /**
    * Adds custom useful matchers to a context.
    *
-   * HACK: Performance may be bad.
-   * In Jasmine 1.3.1, this is designed to be called in 'beforeEach',
-   * so this overwrites the same matchers many times.
+   * HACK: Avoid to overwrite redunduntly.
+   * This may be called more than once in a spec.
    *
-   * @param {jasmine.Spec} spec - Add them to this. NOTE: The type is uncertain.
+   * @param {jasmine.Spec} spec - Add them to this.
    */
   static addMyMatchers(spec) {
     spec.addMatchers({
@@ -21,9 +20,9 @@ export default class {
        * Compares as same as 'toBe'. In addition, this signalize that
        * the behavior is in preparation.
        *
-       * @param  {*} expected - An expected value.
+       * @param  {*}       expected - An expected value.
        * @return {boolean} Is the behavior as expected ?
-       * @this {jasmine.Matchers} NOTE: The type is unsertain.
+       * @this   {jasmine.Matchers} NOTE: The type is unsertain.
        */
       toBeBeforeRun: function(expected) {
 
@@ -54,9 +53,9 @@ export default class {
    * NOTE: Atom's global variables are used.
    * NOTE: Requires that a text editor is active.
    *
-   * @param  {Object} stateBeforeRun
-   * @param  {string} runCommand
-   * @param  {Object} expectedState
+   * @param  {Object}   stateBeforeRun
+   * @param  {string}   runCommand
+   * @param  {Object}   expectedState
    * @param  {Object[]} textVerificationOptions
    * @return {undefined}
    */
